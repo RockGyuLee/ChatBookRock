d4d27ecb36ec5d0f0cc6a0d93b77efc74a76c34f
qowlsdn8007 <qowlsdn8007@gmail.com>
2020-08-26 23:46:11 +0900

    앱 아이콘, 스플래시 추가, 로그인 화면 개선

--- ChatBookRock/android/app/src/main/java/com/chatbookrock/MainActivity.java
+++ ChatBookRock/android/app/src/main/java/com/chatbookrock/MainActivity.java
@@ -1,8 +1,17 @@
 package com.chatbookrock;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, true);
+        super.onCreate(savedInstanceState);
+    }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
