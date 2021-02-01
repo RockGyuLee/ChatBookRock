72befbb5e97f42aa1a1864f7e54744dbfd10fea2
RockGyuLee <rmfoszld@naver.con>
2021-02-02 00:48:08 +0900

    signUp 화면 생성 완료

--- ChatBookRock/src/signup/signup.js
+++ ChatBookRock/src/signup/signup.js
@@ -2,11 +2,16 @@ import React, {useState, useEffect,useReducer} from "react"
 import Styled from 'styled-components/native';
 import Input from "../Component/Input";
 import Button from "../Component/Button";
+import {fbcolDoc} from "../util/firestore";
 import{
   View,
   Image,
   Text
-} from "react-native"
+} from "react-native";
+import auth from "@react-native-firebase/auth";
+import firebase from "@react-native-firebase/app";
+import firestore from "@react-native-firebase/firestore"
+
 
 const Container = Styled.SafeAreaView`
   flex: 1;
@@ -47,16 +52,27 @@ const initialState = {
   name : undefined
 }
 
+const email_pattern = /[@]/gi;
 
-export function SignUp(){
+export function SignUp(props){
   // email 정규식 패턴
-  let email_pattern = /[@]/gi;
+  let navigation = props.navigation;
+  console.log("navigation",navigation);
+  const [bUI, setBUI ] = useState({});
 
   let [state, dispatch] = useReducer(reducer,initialState);
 
   //불일치한 PW 상태 Hook
+  let [discordanceEmail, setDiscordanceEmail] = useState(false);
   let [discordancePW, setDiscordancePW] = useState(false);
   let [discordanceState, setDiscordanceState] = useState(false);
+
+  useEffect(()=>{
+    fbcolDoc('user','basic_user_info')
+    .then(documentSnapshot => {
+      setBUI(documentSnapshot.data());
+    })
+  },[])
 
   const updateState = (type,data) => {
     if(data  == ""){
@@ -67,18 +83,46 @@ export function SignUp(){
 
   const joinMember = () => {
 
-    console.log("email_pattern",email_pattern.test(state.email))
 
-    if(!(state.firstPW == state.secondPW)){
+    if(!(email_pattern.test(state.email))){
+      console.log("email_pattern")
+      setDiscordanceEmail(true);
+      return
+    }
+
+    if(!(state.firstPW == state.secondPW) || state.firstPW.length < 6){
       setDiscordancePW(true);
       return;
     }
 
     if(!(state.email) || !(state.firstPW) || !(state.name) ){
       setDiscordanceState(true);
+      return;
     }
+
+    setDiscordanceEmail(false);
     setDiscordancePW(false);
     setDiscordanceState(false);
+
+    firebase.auth().createUserWithEmailAndPassword(state.email,state.firstPW)
+    .then((userObj)=> {
+      console.log("state")
+      firestore()
+      .collection('user_profile')
+      .doc(userObj.user.uid)
+      .set(Object.assign({},bUI,{
+        user_nm : state.name,
+        user_email : state.email,
+        user_img : userObj.photoURL
+      }));
+      navigation.goBack();
+    })
+    .catch((error) => {
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      alert("회원가입 에러");
+      console.log(errorCode,errorMessage);
+    })
 
   }
   return (
@@ -94,13 +138,21 @@ export function SignUp(){
       <Input
           style={{
             marginTop : 20,
-            marginBottom: 15,
+            marginBottom: 5,
             borderColor: '#89B2E9', 
             borderWidth: 1 
           }}
           placeholder="이메일"
           onChangeText = {updateState.bind(state,"email")}
       />
+      {
+        discordanceEmail
+        ? <Text style={{
+          color : 'red',
+          marginBottom: 15,
+        }}>이메일형식을 다시 확인해주세요.</Text>
+        : <View style={{marginBottom: 15,}}/>
+      }
       <Input
           style={{
             marginBottom: 15,
@@ -123,10 +175,13 @@ export function SignUp(){
       />
       {
         discordancePW 
-        ? <Text style={{
+        ?<>
+         <Text style={{
           color : 'red',
           marginBottom: 15,
-        }}>비밀번호가 일치하지 않습니다. 다시 확인해주세요.</Text>
+        }}>비밀번호가 일치하지 않거나 비밀번호가 너무 짧습니다.</Text>
+        <Text style={{color : 'red'}}>비밀번호는 6글자 이상입니다.</Text>
+        </>
         :<View style={{marginBottom: 15,}}/>
       }
       <Input
